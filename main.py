1edd5ba0fa93d6f5e3631df7247142af184cdf04
brenda dominguez <brendamichelle.d@gmail.com>
2023-10-03 15:01:45 -0500

    Input Testg

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-first_name=input("Please enter your first name:")
-last_name=input("Please enter your last name:")
-print("Hello", first_name, last_name, "nice to meet you!")
+print("Today is a good day to code.")
+print("python is fun")
+print("python's string are easy to use")
+print("Hello " + "World")
+name=input("please enter your name")
+greeting="hello"
+print( greeting + name)
+#if we want a space we can add that too
+print ( greeting +''+name)
 
-first_name=input("Please enter your first name:")
-last_name=input("Please enter your last name:")
-age=input("How old are you " +first_name+ "?")
-print("Hello", first_name, last_name, "nice to meet you!")
-print(first_name, "you were born in the year",2023-int(age))
